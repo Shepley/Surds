64a0a598bb194371a84f50079369712b423ec7ee
T. Shepley <thomasshepley@gmail.com>
2014-06-16 19:14:48 +0100

    Started layout for index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
 	include 'overall.php';
 ?>
-<p>comingsoon</p>
+<div class="container">
+	<div class="row">
+		<div class="col-md-8"><div class="well">
+		<h3>Welcome to Surds.uk</h3>
+		</div></div>
+		<div class="col-md-4"><div class="well"></div>
+	</div>
+</div>
