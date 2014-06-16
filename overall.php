fec335045a315252ce0c6d16e5bce984fab42c0d
T. Shepley <thomasshepley@gmail.com>
2014-06-16 17:03:28 +0100

    Session creation

--- overall.php
+++ overall.php
@@ -1,6 +1,8 @@
 <?php
 	require 'footer.php';
 	require 'nav.php';
+	session_start();
+	$_SESSION['loggedIn'] = 0;
 ?>
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
