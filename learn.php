1e6bba5fe9b3972a32fce75cd4fa48ba707082ca
T. Shepley <thomasshepley@gmail.com>
2014-06-17 19:48:31 +0100

    Created blank well

--- learn.php
+++ learn.php
@@ -1,3 +1,9 @@
 <?php
-
+include 'overall.php';
 ?>
+
+<div class="container">
+	<div class="row">
+		<div class="well align-center"><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>ggggggggggggggg<br><br><br><br><br><br><br><br></div>
+	</div>
+</div>
