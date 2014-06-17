6af0f379240690314eeef028beab309e392a7cba
T. Shepley <thomasshepley@gmail.com>
2014-06-17 19:21:23 +0100

    Added text to homepage

--- index.php
+++ index.php
@@ -5,7 +5,11 @@
 	<div class="row">
 		<div class="col-md-8"><div class="well">
 		<h3>Welcome to Surds.uk</h3>
+		<p>Welcome to the website. Here you will be able to follow your dreams and learn about these magnificient, legendary surds. You sure will have fun!</p>
 		</div></div>
-		<div class="col-md-4"><div class="well"></div>
+		<div class="col-md-4"><div class="well">This project was brought to life by:<br><br><strong>Thomas Shepley</strong> | Web Developer<br><strong>Lewis Walker</strong> | Maths and morale support<br><strong>Joshua Lowe</strong> | Maths<br><strong>Joe Travis</strong> | Maths</div>
 	</div>
 </div>
+	<div class="row">
+		<div class="well align-center">Click here to learn how to do surds and for some exciting questions<div class="pull-right"><button onclick="location.href = '/learn';" class="btn btn-info">Surds away!</button></div><br></div>
+	</div>
