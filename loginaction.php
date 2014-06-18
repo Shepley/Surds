f51c27766f5f73fc5850db6da986de8ca83d6ec1
T. Shepley <thomasshepley@gmail.com>
2014-06-18 17:53:42 +0100

    Added login function

---
+++ loginaction.php
@@ -0,0 +1,25 @@
+<?php
+include 'overall.php';
+include '/register/database.php';
+error_reporting(0);
+$username = $_POST['username'];
+$password = md5($_POST['password']);
+
+$query="SELECT * FROM users";
+$result=mysqli_query($db,$query)
+	or die("Error: ".mysqli_error($db));
+
+while ($row = mysqli_fetch_array($result)){
+
+    if ($username == $row['username'] && $password == $row['password']){
+		$_SESSION['loggedIn'] = 1;
+		$_SESSION['username'] = $row['username']; 
+		header( 'Location: /index');
+	} else {
+		echo '<div class="container"><div class="alert alert-warning alert-danger">
+  <strong>Error 401!</strong> The username or password you entered was incorrect, please try again.<button onClick="location.href=\'/login\'" class="close">Back to login</button>
+</div></div>';
+	}
+}
+
+?>
