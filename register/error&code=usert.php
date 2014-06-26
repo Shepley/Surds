7ad8ffd6ab35c896421b65dedcd125836ec387ad
T. Shepley <thomasshepley@gmail.com>
2014-06-26 17:01:19 +0100

    Created username&displayname taken error pages

---
+++ register/error&code=usert.php
@@ -0,0 +1,8 @@
+<?php
+include 'overall.php';
+?>
+<div class="container"><br><br><br><br><br>
+<div class="alert alert-warning alert-danger">
+  <strong>Sorry!</strong> That username is taken, please try again.<button onClick="location.href='/register'" class="close">Try again</button>
+</div>
+<br><br><br><br><br></div>
