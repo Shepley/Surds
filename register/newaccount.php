7e49af2fb25214cec450c83176008d18a476f5d7
T. Shepley <thomasshepley@gmail.com>
2014-06-26 17:00:57 +0100

    Removed useless/wrong files

--- register/newaccount.php
+++
@@ -1,9 +0,0 @@
-<?php include 'database.php';
-
-$name = $_POST['username'];
-$password = md5($_POST['password']);
-$testscore = "n/a";
-
-mysqli_query($db,"INSERT INTO `users` (username, password, testscore) VALUES ('$name', '$password', '$testscore')");
-
-?>
