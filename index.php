2caa2873e2e612ddd25311224d5db203c6395fdc
T. Shepley <thomasshepley@gmail.com>
2014-06-26 17:05:37 +0100

    Changed weburl on homepage

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
-<?php
-	include 'overall.php';
-?>
+<?php include 'overall.php'; ?>
 <div class="container">
 	<div class="row">
 		<div class="col-md-8"><div class="well">
-		<h3>Welcome to Surds.uk</h3>
+		<h3>Welcome to learnsurds.uk</h3>
 		<p>Welcome to the website. Here you will be able to follow your dreams and learn about these magnificient, legendary surds. You sure will have fun!</p>
 		</div></div>
 		<div class="col-md-4"><div class="well">This project was brought to life by:<br><br><strong>Thomas Shepley</strong> | Web Developer<br><strong>Lewis Walker</strong> | Maths and morale support<br><strong>Joshua Lowe</strong> | Maths<br><strong>Joe Travis</strong> | Maths</div>
