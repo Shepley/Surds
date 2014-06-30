67694280183e4551d10430ad0e87590420e39072
T. Shepley <thomasshepley@gmail.com>
2014-06-30 23:30:45 +0100

    Disabled leaderboards

--- nav.php
+++ nav.php
@@ -6,6 +6,7 @@
     </div>
 
       <?php
+        error_reporting(E_ALL);
         //var_dump($_SESSION);
 
         function curPageName() {
@@ -19,21 +20,23 @@
                   echo ' class="active "';
             }?>><a href="/learn">Learn about Surds</a></li>
         <li<?php if(curPageName() == "leaderboards.php"){
-                  echo ' class="active "';
-            }?>><a href="/leaderboards">Leaderboards</a></li>
+                  echo ' class="active disabled"';
+            } else {
+                  echo ' class="disabled "';
+            }?>><a>Leaderboards</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-      <?php if($_SESSION['loggedIn']){
-     	  echo "<p class='navbar-text'>Signed in as ".$_SESSION['username']."</p>
+      <?php if($_SESSION['loggedIn'] == 1){
+     	  echo "<p class='navbar-text'>Signed in as ".$_SESSION['disp_name']."</p>
      	  <li class='dropdown'>
           <a href='#' class='dropdown-toggle' data-toggle='dropdown'> <span class='glyphicon glyphicon-user'></span> Account <b class='caret'></b></a>
           <ul class='dropdown-menu'>
-            <li><a href='/account/'><span class='glyphicon glyphicon-user'></span> My test scores</a></li>
-            <li class='divider'></li>
+            <li class='disabled'><a>username: ".$_SESSION['username']."</a></li>
             <li><a href='/signout'><span class='glyphicon glyphicon-log-out'></span> Sign out</a></li>
           </ul>
         </li>";
       } else {
+        echo "<li><a href='/register'>Register</a></li>";
         echo "<li";
         include 'loginpagecheck.php';
         echo "><a href='/login'>Login</a></li>";
