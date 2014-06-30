861eef35fe2f5b29033cfa2d8a7f52d72be68ac7
T. Shepley <thomasshepley@gmail.com>
2014-06-30 23:30:32 +0100

    Fixed bug when logging in -> redir to error page

--- loginaction.php
+++ loginaction.php
@@ -4,22 +4,37 @@ include '/register/database.php';
 error_reporting(E_ALL);
 $username = $_POST['username'];
 $password = md5($_POST['password']);
+$ref = $_SERVER["HTTP_REFERER"];
 
-$query="SELECT * FROM users";
-$result=mysqli_query($db,$query)
-	or die("Error: ".mysqli_error($db));
-
-while ($row = mysqli_fetch_array($result)){
-
-    if ($username == $row['username'] && $password == $row['password']){
-		$_SESSION['loggedIn'] = 1;
-		$_SESSION['username'] = $row['username']; 
-		header('Location: /index');
-	} else {
-		echo '<div class="container"><div class="alert alert-warning alert-danger">
-  <strong>Error 401!</strong> The username or password you entered was incorrect, please try again.<button onClick="location.href=\'/login\'" class="close">Back to login</button>
-</div></div>';
-	}
+$dispquery = mysqli_query($db, "SELECT * FROM users WHERE username='".$username."'");
+while($row = mysqli_fetch_array($dispquery)){
+	$_SESSION['disp_name'] = $row['dispname'];
 }
+
+$query= mysqli_query($db, "SELECT * FROM users WHERE username = '".$username."'") or die("Error: ".mysqli_error($db));
+
+if($query->num_rows) {
+
+	while ($row = mysqli_fetch_array($query)){
+
+	    if ($username == $row['username'] && $password == $row['password']){
+			$_SESSION['loggedIn'] = 1;
+			$_SESSION['username'] = $row['username'];
+			$_SESSION['disp_name'] = $row['disp_name'];
+
+				if($ref == "http://localhost/exam"){
+				header('Location: /exam');
+				} else {
+				header('Location: /index');
+		    	}
+		} else {
+			header('Location: /login&error');
+		}
+	}
+} else {
+		header('Location: /login&error');
+}
+
+
 
 ?>
