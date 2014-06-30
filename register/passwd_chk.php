ef426a9f1f7713a6f189fec63fd04635e1db6db0
T. Shepley <thomasshepley@gmail.com>
2014-06-30 23:31:38 +0100

    Registering now fully works

--- register/passwd_chk.php
+++ register/passwd_chk.php
@@ -1,26 +1,32 @@
 <?php
-echo $_POST["username"];
-echo "<br>";
-echo $_POST["password"];
-echo ":";
-echo $_POST["confirm_password"];
 
-if ($_POST["password"] == $_POST["confirm_password"]) {
-	echo '<br>ifsuccess';
-    include 'database.php';
+error_reporting(E_ALL);
+include 'database.php';
 
-	$username = $_POST['username'];
-	$password = md5($_POST['password']);
-	$testscore = "n/a";
+  $username = $_POST['username'];
+  $disp_name = $_POST['disp_name'];
+  $password = md5($_POST['password']);
+  $testscore = "n/a";
 
-	mysqli_query($db,"INSERT INTO `users` (username, password, testscore) VALUES ('$username', '$password', '$testscore')");
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-   $_SESSION['loggedIn'] = 1;
-   header( 'Location: /success');
-} else {
-   header( 'Location: /register/error' );
-}
+$disp_check = mysqli_query($db, "SELECT * FROM users WHERE disp_name = '". $disp_name ."'") or die ('Unable to run userquery:'.mysql_error());
+$user_check = mysqli_query($db, "SELECT * FROM users WHERE username = '". $username ."'") or die ('Unable to run userquery:'.mysql_error());
+var_dump($disp_check);
+var_dump($user_check);
 
+  if(mysqli_num_rows($disp_check) > 0){
+    header('Location: /register/error&code=dispt');
+  } elseif(mysqli_num_rows($user_check) > 0){
+    header('Location: /register/error&code=usert');
+  } else {
+    if ($_POST["password"] == $_POST["confirm_password"]) {
+
+	   mysqli_query($db, "INSERT INTO `users` (username, disp_name, password, testscore) VALUES ('". $username ."', '". $disp_name ."', '". $password ."', '". $testscore ."')");
+      $_SESSION['loggedIn'] = 1;
+      $_SESSION['username'] = $username;
+      $_SESSION['disp_name'] = $disp_name;
+      header( 'Location: /index');
+    } else {
+      header( 'Location: /register/error' );
+    }
+  }
 ?>
