8c2b3b4fccefee5a13293cd7e2aa97a6ca47731e
T. Shepley <thomasshepley@gmail.com>
2014-06-30 23:30:06 +0100

    moved code to seperate include file

--- exam.php
+++ exam.php
@@ -2,54 +2,7 @@
 include 'overall.php';
 
 if($_SESSION['loggedIn']){
-echo '<div class="container">
-		<div class="well">
-			<h3>Grade C/B</h3><small><strong>Simplify these surds</strong></small><br>
-			<br><div class="row">
-			<div class="col-md-2"><strong>1) When $a \ne 0$, there are two solutions to \(ax^2 + bx + c = 0\) and they are
-$$x = {-b \pm \sqrt{b^2-4ac} \over 2a}.$$</strong></div>
-  			<div class="col-md-4"><input type="text" class="form-control" placeholder="Enter Answer" autocomplete="off"></div>
-  			<div class="col-md-3"><strong>2) &radic; 108</strong></div>
-  			<div class="col-md-3"><input type="text" class="form-control" placeholder="Enter Answer" autocomplete="off"></div>
-		</div>
-		<br><div class="row">
-			<div class="col-md-2"><strong>1) &radic; 50</strong></div>
-  			<div class="col-md-4"><input type="text" class="form-control" placeholder="Enter Answer" autocomplete="off"></div>
-  			<div class="col-md-3"><strong>2) &radic; 108</strong></div>
-  			<div class="col-md-3"><input type="text" class="form-control" placeholder="Enter Answer" autocomplete="off"></div>
-		</div>
-		<br><div class="row">
-			<div class="col-md-2"><strong>1) &radic; 50</strong></div>
-  			<div class="col-md-4"><input type="text" class="form-control" placeholder="Enter Answer" autocomplete="off"></div>
-  			<div class="col-md-3"><strong>2) &radic; 108</strong></div>
-  			<div class="col-md-3"><input type="text" class="form-control" placeholder="Enter Answer" autocomplete="off"></div>
-		</div>
-		<br><div class="row">
-			<div class="col-md-2"><strong>1) &radic; 50</strong></div>
-  			<div class="col-md-4"><input type="text" class="form-control" placeholder="Enter Answer" autocomplete="off"></div>
-  			<div class="col-md-3"><strong>2) &radic; 108</strong></div>
-  			<div class="col-md-3"><input type="text" class="form-control" placeholder="Enter Answer" autocomplete="off"></div>
-		</div>
-		<br><div class="row">
-			<div class="col-md-2"><strong>1) &radic; 50</strong></div>
-  			<div class="col-md-4"><input type="text" class="form-control" placeholder="Enter Answer" autocomplete="off"></div>
-  			<div class="col-md-3"><strong>2) &radic; 108</strong></div>
-  			<div class="col-md-3"><input type="text" class="form-control" placeholder="Enter Answer" autocomplete="off"></div>
-		</div>
-		<br><div class="row">
-			<div class="col-md-2"><strong>1) &radic; 50</strong></div>
-  			<div class="col-md-4"><input type="text" class="form-control" placeholder="Enter Answer" autocomplete="off"></div>
-  			<div class="col-md-3"><strong>2) &radic; 108</strong></div>
-  			<div class="col-md-3"><input type="text" class="form-control" placeholder="Enter Answer" autocomplete="off"></div>
-		</div>
-		<br><div class="row">
-			<div class="col-md-2"><strong>1) &radic; 50</strong></div>
-  			<div class="col-md-4"><input type="text" class="form-control" placeholder="Enter Answer" autocomplete="off"></div>
-  			<div class="col-md-3"><strong>2) &radic; 108</strong></div>
-  			<div class="col-md-3"><input type="text" class="form-control" placeholder="Enter Answer" autocomplete="off"></div>
-		</div>
-	</div>
-</div>';
+include 'examhtml1.php';
 } else {
 echo '<div class="container">
 	<div class="row">
