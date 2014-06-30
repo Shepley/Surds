67f190f7d6758fcfe773c93eb64988fa85fde4ce
T. Shepley <thomasshepley@gmail.com>
2014-06-30 23:30:15 +0100

    Added learn para

--- learn.php
+++ learn.php
@@ -4,6 +4,32 @@ include 'overall.php';
 
 <div class="container">
 	<div class="row">
-		<div class="well align-center"><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>ggggggggggggggg<br><br><br><br><br><br><br><br></div>
+		<div class="well align-center">1. &radic;2 * &radic;3 = &radic;(2 * 3) = &radic;6 -- and 6 has no square factors, so this is all you can do.<br>
+<br>
+2. &radic;2 * &radic;10 = &radic;(2 * 10) = &radic;20 <br>
+BUT &radic;20 also equals &radic;( 4 * 5) <br>
+and &radic;4 can be taken to get 2, putting this out in front, but leaving the 5 under the surd, to get the simplified answer of 2&radic;5 <br>
+<br>
+The rule here is: <br>
+Numbers outside are different than numbers under a surd, and must be operated on seperately. <br>
+<br>
+For example: You can add or subtract only if the surds are alike, as in 2&radic;5 + 4&radic;5 = 6&radic;5 <br>
+or 10&radic;6 - 3&radic;6 = 7&radic;6 <br>
+<br>
+When multiplying, you multiply numbers outside seperately from numbers under the surds: <br>
+<br>
+2&radic;6 * 4&radic;5 = 8&radic;30 since 2* 4 = 8 (outside) and &radic;6 * &radic;5 = &radic;30 (inside) <br>
+<br>
+BUT, you also need to look for perfect square factors under surds, so you can further simplify: <br>
+<br>
+3&radic;2 * 7&radic;8 = 21 (3 * 7 outside) * &radic;16 (2 * 8 inside) = 21&radic;16 -- BUT &radic;16 = 4, so the answer actually is, in simplest form, 21 * 4 = 84. <br>
+<br>
+Then, sometimes you can root out a perfect square from under the surd, and leave something under it: <br>
+<br>
+5&radic;6 * 3&radic;12 = 15 ( 5 * 3 outside) * &radic;72 (6 * 12 inside). But &radic;72 = &radic;(36 * 2). And &radic;36 = 6 -- so this goes with the outside numbers to get 15 * 6 = 90 outside. The &radic;2 stays as a surd for your answer in simplest form of : 90&radic;2
+
+<br><br><br>
+<button onclick="location.href = '/exam';" class="btn btn-success">Take the exam!</button>
+</div>
 	</div>
 </div>
