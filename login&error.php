b1dc660a0c7e3e48a63e84582d480bb8087907b2
T. Shepley <thomasshepley@gmail.com>
2014-06-30 23:32:21 +0100

    Login error page created

---
+++ login&error.php
@@ -0,0 +1,7 @@
+<?php include 'overall.php'; ?>
+
+<div class="container"><br><br><br><br><br>
+<div class="alert alert-warning alert-danger">
+  <strong>Yikes!</strong> You entered your username or password wrong.<button onClick="location.href='/login'" class="close">Try again</button>
+</div>
+<br><br><br><br><br></div>
